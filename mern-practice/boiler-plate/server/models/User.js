d8b17038e9d9f94eb954fe65d925b65cb0f91280
knsnyo <isc963@naver.com>
2022-01-12 13:49:18 +0900

    20220112 redux전 저장

---
+++ mern-practice/boiler-plate/server/models/User.js
@@ -0,0 +1,84 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, // 빈칸 제거
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+// 비밀번호 암호화 할게얌.
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // 받은 비밀번호를 암호화하여
+  // 암호화 된 비밀번호와 비교
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  // jsonwebtoken 라이브러리를 사용하여 토큰 만들기
+  const user = this;
+
+  const token = jwt.sign(user._id.toHexString(), "secretToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) {
+      return cb(err);
+    } else {
+      cb(null, user);
+    }
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
