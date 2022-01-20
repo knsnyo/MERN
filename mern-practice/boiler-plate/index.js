65b7ae26ff00793052579c05620e702e7674bece
knsnyo <isc963@naver.com>
2022-01-20 11:06:36 +0900

    복구 좀 해 주세요

--- mern-practice/boiler-plate/index.js
+++ mern-practice/boiler-plate/index.js
@@ -2,12 +2,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const { auth } = require("./middleware/auth");
+
 const config = require("./config/key");
+/* MongoDB에 저장될 User Schema 가져오기 */
 const { User } = require("./models/User");
 
+/* body-parser */
+const bodyParser = require("body-parser");
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -16,6 +17,7 @@ app.use(
 app.use(bodyParser.json());
 
 /* cookie-parser*/
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 /* mongoDB연결 mongoose 사용 */
@@ -34,13 +36,8 @@ app.get("/", (req, res) => {
   res.send("안녕");
 });
 
-// axios test
-app.get("/api/hello", (req, res) => {
-  res.send("안녕하세염");
-});
-
 // 회원가입
-app.post("/api/users/register", (req, res) => {
+app.post("/register", (req, res) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
@@ -54,7 +51,7 @@ app.post("/api/users/register", (req, res) => {
 });
 
 // 로그인
-app.post("/api/users/login", (req, res) => {
+app.post("/login", (req, res) => {
   // 이 사람 있어염?
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -78,42 +75,16 @@ app.post("/api/users/login", (req, res) => {
           return res.status(400).send(err);
         }
 
-        res.cookie("x_auth", user.token).status(200).json({
-          loginSuccess: true,
-          userId: user._id,
-        });
+				res.cookie("x_auth", user.token).status(200).json({
+					loginSuccess: true,
+					userId: user._id,
+				})
       });
     });
   });
 });
 
-app.get("/api/users/auth", auth, (req, res) => {
-  res.status(200).json({
-    _id: req.user._id,
-    isAdmin: req.user.role === 0 ? false : true,
-    isAuth: true,
-    email: req.user.email,
-    name: req.user.name,
-    lastname: req.user.lastname,
-    role: req.user.role,
-    image: req.user.image,
-  })
-});
-
-// 로그아웃
-app.get("/api/users/logout", auth, (req, res) => {
-  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-    if (err) {
-      return res.json({ success: false, err });
-    } else {
-      return res.status(200).send({
-        success: true,
-      });
-    }
-  });
-});
-
 /* 서버 열겠습니다. */
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
