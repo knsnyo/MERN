499b63e116a0d53dc6a262bf7f7f8be891343667
knsnyo <isc963@naver.com>
2022-01-21 17:21:07 +0900

    2020121 navbar 추가 및 auth 오류?

--- mern-practice/boiler-plate/client/src/App.js
+++ mern-practice/boiler-plate/client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from 'react-router-dom'
-import LandingPage from './components/views/LandingPage/LandingPage'
-import LoginPage from './components/views/LoginPage/LoginPage'
-import RegisterPage from './components/views/RegisterPage/RegisterPage'
-import Auth from './hoc/auth';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Auth from "./hoc/auth";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import Navbar from "./components/views/NavBar/NavBar";
+import VideoUploadPage from "./components/views/VideoUploadPage/VideoUploadPage";
 
 function App() {
   const NewLandingPage = Auth(LandingPage, null);
   const NewLoginPage = Auth(LoginPage, false);
   const NewRegisterPage = Auth(RegisterPage, false);
+  const NewVideoUploadPage = Auth(VideoUploadPage, true);
 
   return (
     <Router>
+      <Navbar/>
       <Routes>
-        <Route exact path='/' element={<NewLandingPage/>}/>
-        <Route exact path='/login' element={<NewLoginPage/>}/>
-        <Route exact path='/register' element={<NewRegisterPage/>}/>
+        <Route exact path="/" element={<NewLandingPage />} />
+        <Route exact path="/login" element={<NewLoginPage />} />
+        <Route exact path="/register" element={<NewRegisterPage />} />
+        <Route exact path="/video/upload" element={<NewVideoUploadPage />} />
       </Routes>
     </Router>
   );
