246a97c96e2350a69dbaf8cdeee5f56510842484
knsnyo <isc963@naver.com>
2022-01-23 19:48:32 +0900

    20220123 antd랑 dropzone 잘 모름

--- mern-practice/boiler-plate/client/src/components/views/NavBar/NavBar.js
+++ mern-practice/boiler-plate/client/src/components/views/NavBar/NavBar.js
@@ -8,6 +8,7 @@ function NavBar() {
 				<li><Link to={"./"}>HOME</Link></li>
 				<li><Link to={"./login"}>LOGIN</Link></li>
 				<li><Link to={"./register"}>REGISTER</Link></li>
+				<li><Link to={"./video/upload"}>UPLOAD</Link></li>
 			</ul>
 		</div>
 	)
--- mern-practice/boiler-plate/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ mern-practice/boiler-plate/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,58 @@
 import React from "react";
+import { Typography, Button, Form, message, Input} from "antd";
+import PlusSquareOutlined from "@ant-design/icons"
+import Dropzone from "react-dropzone";
+
+const { TextArea } = Input;
+const { Title } = Typography;
 
 function VideoUploadPage() {
   return (
-	  <div>
-			VideoUploadPage
-	  </div>
-	);
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+      <Form onSubmit>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Dropzone onDrop multiple maxSize>
+            {({ getRootProps, getInputProps }) => {
+              <div
+                style={{
+                  width: "300px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+								<PlusSquareOutlined />
+              </div>;
+            }}
+          </Dropzone>
+          <div>
+            <img src alt />
+          </div>
+        </div>
+        <br />
+        <br />
+        <Input onChange value />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea onChange value />
+        <br />
+        <br />
+        <select onChange>
+          <option value>안녕</option>
+        </select>
+        <Button type="primary" size="large" onClick>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default VideoUploadPage;
