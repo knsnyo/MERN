e564cf289e2f6589fe0050a20e1fb7c8b1085320
knsnyo <isc963@naver.com>
2022-01-30 16:04:27 +0900

    20220130 register

--- mern-blog/server.js
+++ mern-blog/server.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
-mongoose
-  .connect(
-    "?!"
-  )
-  .then(() => console.log("MongoDB connect"));
+mongoose.connect("?!").then(() => console.log("MongoDB connect"));
 
 app.get("/", (req, res) => {
   res.send("Root");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({ 
+        registerSuccess: false 
+      });
+    } else {
+      return res.status(200).json({
+        registerSuccess: true,
+      });
+    }
+  });
 });
 
 const port = 5000;
