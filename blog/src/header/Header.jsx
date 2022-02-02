52bb3d2f6296753fb0eb6bbc56dc31b74f39845e
knsnyo <isc963@naver.com>
2022-02-02 15:40:41 +0900

    20220202

---
+++ blog/src/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css";
+import HeaderImg from "./img/profile.png";
+
+export default function Header() {
+	return (
+		<div className="header">
+		  <div className="headerTitles">
+				<span className="headerTitleSm">React & Node</span>
+				<span className="headerTitleLg">Blog</span>
+			</div>
+			<img
+			  className="headerImg"
+				src={HeaderImg}
+				alt=""
+			/>
+		</div>
+	);
+}
