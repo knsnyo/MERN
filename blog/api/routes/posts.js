f39c74c9b7c04f65f7b7996d63da0d85174846ea
knsnyo <isc963@naver.com>
2022-02-12 16:10:08 +0900

    20220212 update post

--- blog/api/routes/posts.js
+++ blog/api/routes/posts.js
@@ -6,31 +6,36 @@ const bcrypt = require("bcrypt");
 //CREATE POST
 router.Post("/", async (req, res) => {
   const newPost = new Post(req.body);
-	try {
-		const savedPost = await newPost.save();
-		res.status(200).json(savedPost); 
-	} catch (err) {
-		res.status(500).json(err);
-	}
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //UPDATE
-router.delete("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username == req.body.username) {
       try {
-        await Post.deleteMany({ username: user.username });
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted.");
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
       } catch (err) {
         res.status(500).json(err);
       }
-    } catch (err) {
-      res.status(404).json("User not found");
+    } else {
+      res.status(401).json("You can update only your post");
     }
-  } else {
-    res.status(401).json("You can delete only your account");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
