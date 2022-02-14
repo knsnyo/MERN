10f94c09de9732d345389797b5dc8598ae920327
knsnyo <isc963@naver.com>
2022-02-14 20:09:05 +0900

    20220214 get post

--- blog/api/routes/posts.js
+++ blog/api/routes/posts.js
@@ -43,12 +43,12 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if(post.username === req.body.username) {
+    if (post.username === req.body.username) {
       try {
         await post.delete();
         res.status(200).json("Post deleted.");
       } catch (err) {
-        res.status(500).json(err)
+        res.status(500).json(err);
       }
     } else {
       res.status(401).json("You can delete only your post.");
@@ -61,9 +61,18 @@ router.delete("/:id", async (req, res) => {
 //GET POST
 router.get("/:id", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL POST
+router.get("/?user=", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
   } catch (err) {
     res.status(500).json(err);
   }
