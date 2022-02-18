157cb51aa37ad440b7cb9510acd5dc8e6698909f
knsnyo <isc963@naver.com>
2022-02-18 11:14:41 +0900

    20220218 image upload

--- blog/api/index.js
+++ blog/api/index.js
@@ -4,8 +4,9 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts")
+const postRoute = require("./routes/posts");
 const categoriesRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -14,6 +15,20 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "image");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File upload");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
