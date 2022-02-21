ee7cad3bf2b8be3033f61c44d5aeb0ca23658a6d
knsnyo <isc963@naver.com>
2022-02-21 14:12:34 +0900

    20220221 single post

--- blog/client/src/components/singlePost/SinglePost.jsx
+++ blog/client/src/components/singlePost/SinglePost.jsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 import "./singlePost.css";
 import Music from "./img/music.png"
 
 export default function SinglePost() {
+  const location = useLocation();
+  //console.log(location);
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+
+  useEffect(()=>{
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      console.log(res);
+    };
+    getPost();
+  }, [path]);
+
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
+        {post.photo && (<img src={post.photo} alt="" className="singlePostImg"/>)}
         <img src={Music} alt="" className="singlePostImg" />
         <h1 className="singPostTitle">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          {post.title}
           <div className="singlePostEdit">
             <i className="singlePostIcon far fa-edit"></i>
             <i className="singlePostIcon far fa-trash-alt"></i>
@@ -15,15 +32,12 @@ export default function SinglePost() {
         </h1>
         <div className="singlePostInfo">
           <span className="singlePostAuthor">
-            Autor: <b>knsn</b>
+            Autor: <b>{post.username}</b>
           </span>
-          <span className="singlePostDate">1 hours ago</span>
+          <span className="singlePostDate">{new Date(post.createAt).toDateString}</span>
         </div>
         <p className="singlePostDescription">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex minima
-          architecto porro dolor maiores quo expedita. Itaque non, inventore
-          blanditiis possimus voluptatem obcaecati ducimus aliquam harum ipsam
-          ut, quia maiores?
+          {post.description}
         </p>
       </div>
     </div>
