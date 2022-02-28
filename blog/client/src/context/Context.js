5cd179f64c9d59ec45c4613e20d9be8a96c2cb19
knsnyo <isc963@naver.com>
2022-03-01 06:14:49 +0900

    reducer logout and error

--- blog/client/src/context/Context.js
+++ blog/client/src/context/Context.js
@@ -9,7 +9,7 @@ const INITIAL_STATE = {
 
 export const Context = createContext(INITIAL_STATE);
 export const ContextProvider = ({children}) => {
-	const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+	const [ state, dispatch ] = useReducer(Reducer, INITIAL_STATE);
 
 	useEffect(()=>{
 		localStorage.setItem("user", JSON.stringify(state.user));
