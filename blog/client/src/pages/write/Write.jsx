1fb2d3fc6173179f63e07fbea7870ee473c07fb2
knsnyo <isc963@naver.com>
2022-03-04 15:56:51 +0900

    20220304 upload img 1

--- blog/client/src/pages/write/Write.jsx
+++ blog/client/src/pages/write/Write.jsx
@@ -1,11 +1,42 @@
 import "./write.css";
 import WriteBackground from "../../img/background.png";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { Context } from "../../context/Context";
 
 export default function Write() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState(null);
+  const { user } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username: user.username,
+      title: "title",
+      description: "description",
+    };
+    if (file) {
+      const data = FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      newPost.photo = filename;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      const res = await axios.post("/posts", newPost);
+      window.location.replace("/post/" + res.data._id);
+    } catch (err) {}
+  };
+
   return (
     <div className="write">
-			<img className="writeImg" src={WriteBackground} alt=""/>
-      <form className="writeForm">
+      <img className="writeImg" src={WriteBackground} alt="" />
+      <form className="writeForm" onSubmit={handleSubmit}>
         <div className="writeFormGroup">
           <label htmlFor="fileInput">
             <i className="writeIcon fas fa-plus-square"></i>
@@ -25,7 +56,9 @@ export default function Write() {
             className="writeInput writeText"
           />
         </div>
-        <button className="writeSubmit">Publish</button>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
       </form>
     </div>
   );
