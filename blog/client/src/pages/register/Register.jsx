872e260a8adf8d6ae455851ac1db2f88508dd1b2
knsnyo <isc963@naver.com>
2022-03-16 19:47:56 +0900

    20220316 register

--- blog/client/src/pages/register/Register.jsx
+++ blog/client/src/pages/register/Register.jsx
@@ -12,7 +12,7 @@ export default function Register() {
     e.preventDefault(); // no refreshing
     setError(false);
     try {
-      const res = await axios.post("/api/auth/register", {
+      const res = await axios.post("/auth/register", {
         username,
         email,
         password,
